a585d41b23a35a01ad3573175d3d303b2b24cca6
camit4777 <your-camit4777@gmail.com>
2026-03-22 22:48:59 +0530

    Update 3

---
+++ Arithmatic.py
@@ -0,0 +1,11 @@
+# HackerRank Problem: Arithmetic Operators
+
+# HackerRank Problem: Arithmetic Operators
+
+if __name__ == "__main__":
+    a = int(input())   # First integer
+    b = int(input())   # Second integer
+
+    print(a + b)   # Sum
+    print(a - b)   # Difference
+    print(a * b)   # Product
